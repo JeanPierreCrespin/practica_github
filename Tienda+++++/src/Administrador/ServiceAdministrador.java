c13719ac20ae5dc2ce5fe0a996547edb36fb8267
Cristian Rivera <cristian@costaflores.com>
2020-10-16 20:30:09 -0300

    agrege administracion

---
+++ Tienda+++++/src/Administrador/ServiceAdministrador.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Administrador;
+
+/**
+ *
+ * @author cristian
+ */
+public class ServiceAdministrador {
+    
+    
+    public void MantenimientoYcontrol(){}  
+    public void compras(){}
+    public void pedido(){}
+}
